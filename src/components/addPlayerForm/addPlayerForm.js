05fdd4d2dcdd73bc86b1c73c8eae3756ef59220f
David Whitmore <davidwhit818@gmail.com>
2023-11-01 12:53:21 -0700

    add gap and validate add player

--- src/components/addPlayerForm/addPlayerForm.js
+++ src/components/addPlayerForm/addPlayerForm.js
@@ -1,11 +1,21 @@
 import { Form, Button } from "react-bootstrap";
+import { useState,  } from "react";
 
 function AddPlayerForm(props) {
+  const [isNewPlayerNameValid, setIsNewPlayerNameValid] = useState(false)
+
+  const validatePlayerName = (name) => {
+    if(!name){
+      setIsNewPlayerNameValid(true)
+    }
+    else(setIsNewPlayerNameValid(false))
+  }
+
   return (
     <div className="addNameForm">
       <Form className="addName" onSubmit={(e) => props.addPlayer(e)}>
-        <Form.Control id="name" type="text" placeholder="Player Name" />
-        <Button type="submit" variant="warning">
+        <Form.Control id="name" type="text" placeholder="Player Name" class={isNewPlayerNameValid ? '.is-valid' : 'is-invalid'} onChange={(e)=> validatePlayerName(e.target.value)}/>
+        <Button type="submit" variant="warning" disabled={isNewPlayerNameValid}>
           Add Player
         </Button>
       </Form>
