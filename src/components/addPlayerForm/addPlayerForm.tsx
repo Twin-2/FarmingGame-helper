171ca5909fd4513ae47b83de21bfcb98b2b1e472
David Whitmore <davidwhit818@gmail.com>
2024-01-24 10:43:37 -0800

    cleanup-player add form

--- src/components/addPlayerForm/addPlayerForm.tsx
+++ src/components/addPlayerForm/addPlayerForm.tsx
@@ -1,5 +1,6 @@
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Tooltip, OverlayTrigger } from "react-bootstrap";
 import { useState,  } from "react";
+import Feedback from "react-bootstrap/esm/Feedback";
 
 type AddPlayerFormProps = {
   addPlayer: (e:React.BaseSyntheticEvent) => void;
@@ -7,19 +8,36 @@ type AddPlayerFormProps = {
 
 function AddPlayerForm(props: AddPlayerFormProps) {
   const [isNewPlayerNameValid, setIsNewPlayerNameValid] = useState(false)
+  console.log(isNewPlayerNameValid)
 
   const validatePlayerName = (name: string) => {
     if(!name){
+      setIsNewPlayerNameValid(false)
+      console.log('1')
+    }
+    else{
+      console.log('2')
       setIsNewPlayerNameValid(true)
     }
-    else(setIsNewPlayerNameValid(false))
   }
+
+  const tooltip = (
+    <Tooltip id="tooltip">
+     Please enter a name
+    </Tooltip>
+  );
 
   return (
     <div className="addNameForm">
       <Form className="addName" onSubmit={(e) => props.addPlayer(e)}>
-        <Form.Control id="name" type="text" placeholder="Player Name" className={isNewPlayerNameValid ? '.is-valid' : 'is-invalid'} onChange={(e)=> validatePlayerName(e.target.value)}/>
-        <Button type="submit" variant="warning" disabled={isNewPlayerNameValid}>
+          <OverlayTrigger placement="bottom" overlay={tooltip} trigger={isNewPlayerNameValid ? 'focus': 'hover'}>
+            <Form.Control id="name" type="text" placeholder="Player Name" className={isNewPlayerNameValid ? '.is-valid' : 'is-invalid'} onChange={(e)=> validatePlayerName(e.target.value)}/>
+          </OverlayTrigger>
+          <Feedback type="invalid" tooltip={true}>
+            Please enter a name!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+          </Feedback>
+
+        <Button type="submit" variant="warning" disabled={!isNewPlayerNameValid}>
           Add Player
         </Button>
       </Form>
